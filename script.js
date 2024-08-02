13cd29d94b6740d5c5952b39e28f0bf2dbb519db
ashfaqur rashid <135977170+ashfaqur-rashidmo@users.noreply.github.com>
2024-08-02 19:08:00 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/*Typing Animation*/
+var typed = new Typed(".typing",{
+   strings:["","Web Designer","Web Developer","Graphic Designer","YouTuber"],
+   typeSpeed:100,
+   backSpeed:60,
+   loop:true
+  
+})
